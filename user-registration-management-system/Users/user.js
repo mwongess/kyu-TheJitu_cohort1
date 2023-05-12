c9b6e929e3f7c57eeafbecb13ee035f15578faa8
mwongess <amosmwongelah@gmail.com>
2023-05-12 18:34:48 +0300

    Delete user

--- user-registration-management-system/Users/user.js
+++ user-registration-management-system/Users/user.js
@@ -1,15 +1,11 @@
 // Data displayed  when we launch the page
-
+const json_data = localStorage.getItem("users");
+const data = JSON.parse(json_data);
 
 // Create, read, update, delete methods to manipulate the data
 
 const displayUsers = () => {
   const outlet = document.querySelector(".outlet");
-
-  const json_data = localStorage.getItem('users')
-
-  const data = JSON.parse(json_data)
-
   data.forEach((user) => {
     outlet.innerHTML += `
             <tr>
@@ -19,7 +15,7 @@ const displayUsers = () => {
             <button class="update">Update</button>
             </td>
             <td>
-            <button class="delete">Delete</button>
+            <button onclick="deleteUser('${user.id}')" class="delete">Delete</button>
             </td>
             </tr>
     `;
@@ -27,6 +23,14 @@ const displayUsers = () => {
 };
 
 const updateUser = (id) => {};
-const deleteUser = (id) => {};
+
+const deleteUser = (id) => {
+  const data_after_deleted = data.filter((user) => {
+    return user.id != id;
+  });
+  console.log(data_after_deleted);
+  localStorage.setItem("users", JSON.stringify(data_after_deleted));
+  location.reload();
+};
 
 displayUsers();
